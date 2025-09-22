c4443a120d2c4ed685aa0503bd3057544936de03
tuanduy010806 <24020109@vnu.edu.vn>
2025-09-22 15:14:22 +0700

    add file Introduction.java vao HW1

---
+++ HW1/Introduction.java
@@ -0,0 +1,10 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Lê Hồng Tuấn Duy\t" + 24020109 + "\tK69I-IT1\t" + "tuanduy010806\t" + "24020109@vnu.edu.vn");
+        for (int i = 9; i >= 1; i--) {
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
